4402b3f89cb3179d19de18d3c7b70d35c8c0a5e6
Nomura Kei <nomura.kei.root@gmail.com>
2026-03-27 03:21:41 +0900

    起動時に、squid.conf も更新するよう変更。

--- start.bat
+++ start.bat
@@ -9,6 +9,7 @@ set "WIN_CONF_PATH=%~dp0conf"
 for /F "usebackq delims=" %%p in (`wsl -d AlpineProxy wslpath -a -u "%WIN_CONF_PATH%"`) do set "WIN_CONF_PATH=%%p"
 
 wsl -d AlpineProxy -u root --exec /bin/mkdir -p /etc/squid/conf.d
+wsl -d AlpineProxy -u root --exec /bin/cp "%WIN_CONF_PATH%/squid.conf" /etc/squid/
 wsl -d AlpineProxy -u root --exec /bin/cp "%WIN_CONF_PATH%/proxy.conf" /etc/squid/conf.d/
 
 wsl --terminate AlpineProxy
